47ab690a1c9738580b8c998001563d7c489e280c
swimly <979741120@qq.com>
2025-10-22 21:11:46 +0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * @ldesign/timeline - 时间轴
+ */
+export class Timeline {
+  constructor(private container: HTMLElement) { }
+  render() { console.info('Timeline rendering') }
+}
+export function createTimeline(container: HTMLElement) { return new Timeline(container) }
+
